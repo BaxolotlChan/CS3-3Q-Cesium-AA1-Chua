5af0ca7f2b4789789c52f0a906bcd244601e78b6
BaxolotlChan <bkchua22@src.pshs.edu.ph>
2025-01-23 08:56:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let volumeOfSphere = 4/3*pi*r**3;
 let circumferenceOfSphere = 2*pi*r;
 let surfaceAreaOfSphere = 4*pi*r**2;
 
-document.write("The radius of the sphere is " + r + "." '<br>');
-document.write("The circumference is " + circumferenceOfSphere + "." '<br>');
-document.write("The surface area is " + surfaceAreaOfSphere + "." '<br>');
-document.write("The volume is " + volumeOfSphere + "." '<br>');
+document.write("The radius of the sphere is " + r + "." + '<br>');
+document.write("The circumference is " + circumferenceOfSphere + "." + '<br>');
+document.write("The surface area is " + surfaceAreaOfSphere + "." + '<br>');
+document.write("The volume is " + volumeOfSphere + "." + '<br>');
